838db867df0026222d4eaaf2e97a405a01dcb347
Jugal Deepak Chauhan <43086157+jugal-chauhan@users.noreply.github.com>
2022-05-12 12:20:59 +0530

    removing old files

--- lib/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-// Colors
-const bgColor = Color(0xff191720);
-const bgColor1 = Color(0xFF12100E);
-const bgColor2 =  Color(0xFF2B4162);
-
-// TextStyles
